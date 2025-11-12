59873df2a1ffd9497111b3460740e007a232ff71
Oluwadt <dtonilede@gmail.com>
2025-11-12 12:49:05 +0100

    Created counting functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
+import { useState } from "react"
+
 function App() {
+  const [count, setCount] = useState(0)
+
+  function addCount() {
+    setCount(count+1)
+  }
 
   return (
     <>
-      
+      <h1>Count: {count}</h1>
+      <button onClick={()=>addCount()}>Add Count</button>
     </>
   )
 }
